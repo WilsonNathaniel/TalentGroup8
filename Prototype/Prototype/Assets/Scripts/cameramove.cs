d4f4bfb5066015c90c752a3fe437f4b59642ed7a
WilsonNathaniel <wilsonnathaniel955@gmail.com>
2021-12-28 20:26:44 +0700

    Perapihan dan hapus asset lama
    gw rapihin camera ama doornya, swtichnya collidernya kukecilin, buat prefab untuk door dan switch.

--- Prototype/Prototype/Assets/Scripts/cameramove.cs
+++ Prototype/Prototype/Assets/Scripts/cameramove.cs
@@ -6,17 +6,18 @@ public class cameramove : MonoBehaviour
 {
     private GameObject camera;
     private bool activated = false;
+    [SerializeField] private float moveby = 25;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         camera = GameObject.Find("Main Camera");
         if (activated == false)
         {
-            camera.transform.Translate(25, 0, 0);
+            camera.transform.Translate(moveby, 0, 0);
             activated = true;
         }
         else
         {
-            camera.transform.Translate(-25, 0, 0);
+            camera.transform.Translate(-moveby, 0, 0);
             activated = false;
         }
     }
