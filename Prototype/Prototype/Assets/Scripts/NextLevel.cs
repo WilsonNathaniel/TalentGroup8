fcac6de87dfd6068973391cf62135a8e67aba353
capekbuatusername <seanmattz02@gmail.com>
2021-12-29 10:58:04 +0700

    add next level script

---
+++ Prototype/Prototype/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GoNextLevel();
+        
+    }
+    public void GoNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
