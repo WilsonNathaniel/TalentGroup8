0ee707f77014ee62ec22915241857e089dfad7e4
capekbuatusername <seanmattz02@gmail.com>
2022-01-04 14:44:48 +0700

    update trigger switch script

--- Prototype/Prototype/Assets/Scripts/TriggerSwitch.cs
+++ Prototype/Prototype/Assets/Scripts/TriggerSwitch.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class TriggerSwitch : MonoBehaviour
 {
     public bool isSwitched = false;
+    private bool isPressed = false;
+  //  private bool isTriggering = false;
 
     // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
+ 
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+     //   Debug.Log("triggering");
+        if (Input.GetKey(KeyCode.Q))
         {
+            isPressed = true;
+     //       Debug.Log("q pressed");
+        }
+
+        if (collision.gameObject.tag == "Player" && isPressed == true)
+        {
+
             isSwitched = true;
         }
     }
