516d32d7c8f3f5dd577142a494dccb41896817dd
capekbuatusername <seanmattz02@gmail.com>
2022-01-14 21:06:23 +0700

    .

--- Flipborg/Flipborg/Assets/Scripts/Hazard.cs
+++ Flipborg/Flipborg/Assets/Scripts/Hazard.cs
@@ -7,7 +7,6 @@ public class Hazard : MonoBehaviour
 {
     // Start is called before the first frame update
 
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
